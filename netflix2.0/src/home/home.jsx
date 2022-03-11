9e34d093b937e289dfa11538799772800bb837ed
BretsW <59100941+Ophidium@users.noreply.github.com>
2022-03-11 15:43:26 +0100

    Mostly added the navbar

---
+++ netflix2.0/src/home/home.jsx
@@ -0,0 +1,49 @@
+import Navbar from "../components/navbar/Navbar"
+import "./home.scss"
+
+export default function home() {
+  return (
+    <div className='home'>
+      <Navbar/>
+      <img width="100%" src="https://i.imgur.com/r8PaTmf.png" alt="" />
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis aut enim, accusamus, accusantium dolore, voluptatum beatae doloremque qui quod id provident! Id nam libero quas, sit sequi magni architecto consectetur.
+
+    </div>
+  )
+}
